5709db58aeac04cc58cf972b4a63138510aaecb7
gdfgdfgd <csadasdaso>
2020-02-29 04:56:11 -0500

    dojo problema con permisos cors

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require(cors);
+const awsMiddleware = require('aws-serverless-express/middleware');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(awsMiddleware.eventContext());
+
+app.use('/',routes);
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const awsSE = require('aws-serverless-express');
+
+const app = require('./app')
+const server = awsSE.createServer(app);
+
+exports.handler =(event, context)=> {
+awsSE.proxy(server,event,context);
+}
