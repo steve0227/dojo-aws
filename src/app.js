dda4ccee5f04240271779995871a9621d57ae33e
steve0227 <39034419+steve0227@users.noreply.github.com>
2020-02-29 10:40:46 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require(cors);
+const cors = require('cors');
 const awsMiddleware = require('aws-serverless-express/middleware');
 const routes = require('./routes');
 
